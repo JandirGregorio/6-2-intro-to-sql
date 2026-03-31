2dd93d17404b53761285f2a3af15c716ef924329
Jandir <jandirgregorio@icloud.com>
2026-03-31 14:29:38 -0400

    basic sql commands

--- queries.sql
+++ queries.sql
@@ -7,33 +7,43 @@
 ----------------------------------------
 
 -- Get all films
+-- retrieve all data from the films table
 SELECT * FROM films;
 
 -- Get only title and director
+-- retrieve the title and director from the films table
 SELECT title, director FROM films;
 
 -- Get all sci-fi films
+-- retrieve all sci-fi films records
 SELECT * FROM films WHERE genre = 'sci-fi';
 
 -- Films released after 2015
+-- retrieve the films records released after the year 2015
 SELECT * FROM films WHERE year > 2015;
 
 -- Films whose title starts with 'The'
+-- get all film records that start with 'The'
 SELECT * FROM films WHERE title LIKE 'The%';
 
 -- Horror or thriller films
+-- get film records whose genere is horror or thriller
 SELECT * FROM films WHERE genre = 'horror' OR genre = 'thriller';
 
 -- Sci-fi films released after 2010
+-- get film records whose genre is sci-fi and was releaser after 2010
 SELECT * FROM films WHERE genre = 'sci-fi' AND year > 2010;
 
 -- All films newest to oldest
+-- get films from newest to oldest sorted by year
 SELECT * FROM films ORDER BY year DESC;
 
 -- Top 3 most recent films
+-- get the 3 most recent films sorted by year
 SELECT * FROM films ORDER BY year DESC LIMIT 3;
 
 -- All films alphabetically by title
+-- get film records sorted alphabetically
 SELECT * FROM films ORDER BY title ASC;
 
 ----------------------------------------
@@ -41,10 +51,12 @@ SELECT * FROM films ORDER BY title ASC;
 ----------------------------------------
 
 -- Add a single film
+-- insert values (film) to db
 INSERT INTO films (title, director, year, genre)
 VALUES ('Dune', 'Denis Villeneuve', 2021, 'sci-fi');
 
 -- Add multiple films at once
+-- insert two films to the db
 INSERT INTO films (title, director, year, genre)
 VALUES
   ('Arrival',  'Denis Villeneuve', 2016, 'sci-fi'),
@@ -58,11 +70,13 @@ SELECT * FROM films ORDER BY film_id;
 ----------------------------------------
 
 -- Update a genre by title
+-- update Moonlight's genre to drama
 UPDATE films
 SET genre = 'drama'
 WHERE title = 'Moonlight';
 
 -- Update multiple columns by ID
+-- update the title and year by id
 UPDATE films
 SET title = 'Everything Everywhere All at Once', year = 2022
 WHERE film_id = 6;
@@ -75,6 +89,7 @@ SELECT * FROM films WHERE film_id = 6;
 ----------------------------------------
 
 -- Delete a specific film by ID
+-- delete the film with id of 7
 DELETE FROM films WHERE film_id = 7;
 
 -- Verify the delete
